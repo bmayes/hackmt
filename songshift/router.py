de695d9df451a83f7979f29c9ebd394a892052f6
Bryan Mayes <bmayes@eventbrite.com>
2016-01-29 21:23:14 -0600

    New files

--- songshift/router.py
+++ songshift/router.py
@@ -1,4 +1,14 @@
+import os
+from flask import render_template, send_from_directory, redirect, url_for, flash, session, g, jsonify, request, abort
+from functools import wraps
+from songshift import app, db, theLoginMgr
 
+from flask.ext.login import login_required, current_user, login_user, logout_user
+
+
+# @theLoginMgr.user_loader
+# def load_user(userid):
+#     return User.query.get(userid)
 
 
 """ -------------------------------------------------------
