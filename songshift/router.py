7ce3f00434ff65019739f336e8182d7b7ba4c18e
Corey Sery <coreysery@gmail.com>
2016-01-30 08:48:35 -0600

    pulls random song on every load

--- songshift/router.py
+++ songshift/router.py
@@ -20,26 +20,41 @@ import random
 def index():
     navbar = {}
 
+
+
+
+    track = getNewTrack()
+    body = {
+        "song_title" : track.title,
+        "artist" : track.user['username'],
+        "artwork_url" : track.artwork_url,
+        "song_id" : track.id
+    }
+
+    return render_template('index.html', navbar=navbar, body=body)
+
+
+def getNewTrack():
+
     # client id provided by soundcloud
     SC_CLIENT_ID = '8173b8c45f9a7ed733c87095efde5b73'
 
-    SEARCH_AHEAD = 4 #num of items to preload
+    SEARCH_AHEAD = 100 #num of items to preload
+    valid_track_types = 'original, remix, recording, live'
+    users_genres = "rock, electronic"
 
     client = soundcloud.Client(client_id=SC_CLIENT_ID)
 
     # fetch random track
-    track_id = random.randint(100,1000)
-    track = client.get('/tracks/%d' % track_id)
+    # track_id = random.randint(100,1000)
+    # track = client.get('/tracks/%d' % track_id)
 
-    tracks = client.get('/tracks', limit=SEARCH_AHEAD)
+    tracks = client.get('/tracks',
+                        limit=SEARCH_AHEAD,
+                        types=valid_track_types,
+                        genre=users_genres)
 
-    for t in tracks:
-        print t.title
+    rand_num = random.randint(0, SEARCH_AHEAD-1)
+    rand_track = tracks[rand_num]
 
-    body = {
-        "song_title" : track.title,
-        "artist" : track.user['username'],
-        "artwork_url" : track['artwork_url']
-    }
-
-    return render_template('index.html', navbar=navbar, body=body)
+    return rand_track
