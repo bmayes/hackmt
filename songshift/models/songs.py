f98a396a1f17b3071368574f348d6aef8304cf6b
james <jamesdickensononline@gmail.com>
2016-01-30 15:53:21 -0600

    Added framework for database

--- songshift/models/songs.py
+++ songshift/models/songs.py
@@ -5,7 +5,7 @@ class Song(db.Model):
     __tablename__ = "songs"
     id = db.Column(db.Integer, primary_key=True)
     songid = db.Column(db.Integer)
-    affinity = db.Column(db.Boolean)
+    affinity = db.Column(db.Integer)
     genre = db.Column(db.String(150))
     def __repr__(self):
-        return '<Song %r %r>' % (self.songid, self.affinity)
+        return '<Song %r %r>' % (self.genre, self.affinity)
