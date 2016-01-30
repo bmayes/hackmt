a98d2621a223590c8f534acb9fc74f2d3e18230c
balani29 <adriano.balani1@gmail.com>
2016-01-30 10:18:02 -0600

    Added like, dislike, swipeleft, swiperight buttons.

--- songshift/static/js/function.js
+++ songshift/static/js/function.js
@@ -2,7 +2,7 @@
  * Created by coreysery on 2/2/15.
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('.circle1').click(function AnimateRotate(angle) {
         // caching the object for performance reasons
@@ -14,7 +14,7 @@ $(document).ready(function() {
         // (starts from `0` to `angle`), you can name it as you want
         $elem.animate({deg: angle}, {
             duration: 160000,
-            step: function(now) {
+            step: function (now) {
                 // in the step-callback (that is fired each step of the animation),
                 // you can use the `now` paramter which contains the current
                 // animation-position (`0` up to `angle`)
@@ -30,15 +30,16 @@ $(document).ready(function() {
         angle = -40;
         angle += 65;
 
+
         // we use a pseudo object for the animation
         // (starts from `0` to `angle`), you can name it as you want
         $elem1.animate({deg: angle}, {
             duration: 160000,
-            step: function(now) {
+            step: function (now) {
                 // in the step-callback (that is fired each step of the animation),
                 // you can use the `now` paramter which contains the current
                 // animation-position (`0` up to `angle`)
-                var now1 = now -40;
+                var now1 = now - 40;
                 $elem1.css({
                     transform: 'rotate(' + now1 + 'deg)'
 
@@ -48,9 +49,59 @@ $(document).ready(function() {
         });
     });
 
-    $('.vinyl').on('swipe', function() {
-        $(this).hide();
-    })
+    // $('.vinyl').on('swipe', function() {
+    //   $(this).hide();
+    //})
 
+    //$('.needle').on('click', function() {
+    //  $(this).hide();
+    //})
 
-});
+     $('.vinyl').on('swipeleft', function(e){
+    data = {
+        song_id: 24234,
+        like: false
+    };
+    vote(data)
+    });
+    $('.vinyl').on('swiperight', function(e){
+      data = {
+        song_id: 24235,
+        like: true
+    };
+    vote(data)
+    });
+
+    $('#like').on('click', function () {
+        console.log("like");
+        data = {
+        song_id: 24235,
+        like: true
+    };
+    vote(data)
+        //$(this).hide();
+    });
+
+    $('#dislike').on('click', function () {
+        console.log("dislike");
+        //$(this).hide();
+        data = {
+        song_id: 24235,
+        like: false
+    };
+    vote(data)
+    });
+
+    function vote(data) {
+        $.ajax({
+            type: "POST",
+            url: "/newsong",
+            data: data,
+            cache: false,
+            success: function (data1) {
+                console.log(data1);
+            }
+        });
+    }
+
+    });
